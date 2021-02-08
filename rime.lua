6e9a0ac005856dea8847b58798257f894618f1d4
yeweijian <yeweijian@eyugame.com>
2021-02-08 16:13:49 +0800

    优化脚本

--- rime.lua
+++ rime.lua
@@ -2,11 +2,22 @@ function date_translator(input, seg)
    if (input == "date") then
       --- Candidate(type, start, end, text, comment)
       yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
+      yield(Candidate("date", seg.start, seg._end, os.date("%m-%d"), ""))
+      yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
    end
+
    if (input == "time") then
       --- Candidate(type, start, end, text, comment)
       yield(Candidate("time", seg.start, seg._end, os.date("%Y-%m-%d %H:%M:%S"), ""))
       yield(Candidate("time", seg.start, seg._end, os.time(), "时间戳"))
+   end
+
+   if (input == "week") then
+      local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
+      yield(Candidate("week", seg.start, seg._end, "周"..weakTab[tonumber(os.date("%w")+1)], ""))
+      yield(Candidate("week", seg.start, seg._end, "星期"..weakTab[tonumber(os.date("%w")+1)], ""))
    end
 end
 
